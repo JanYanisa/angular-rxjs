fe20c876c5af2fce93d251f71651c9b6a72539c1
yanisa.p <yanisa.p@gofive.co.th>
2022-11-21 12:15:31 +0700

    activate with click use EventEmitter (not recommend)

---
+++ src/app/user.service.ts
@@ -0,0 +1,8 @@
+import {Injectable, EventEmitter} from '@angular/core'
+import { Subject } from 'rxjs'
+
+@Injectable({providedIn: 'root'}) //with this, there's no need to be added in the providers: [], in app.Module
+export class UserService {
+    activatedEmitter = new EventEmitter<boolean>()
+    // activatedEmitter = new Subject<boolean>()
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user',
@@ -8,13 +9,18 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class UserComponent implements OnInit {
   id: number;
-
-  constructor(private route: ActivatedRoute) {
+  click:boolean = false;
+  constructor(private route: ActivatedRoute, private userService: UserService) {
   }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
     });
+  }
+
+  onActivate() {
+    this.click = !this.click
+    this.userService.activatedEmitter.emit(this.click)
   }
 }
